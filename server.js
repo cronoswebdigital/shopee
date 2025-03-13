0531310e736de4b4ca674bd711a7ed57d87207a3
Leonardo Rodrigues <krsolucoes@gmail.com>
2025-03-13 12:43:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const crypto = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post('/generate-signature', (req, res) => {
     const { appID, senha } = req.body;
